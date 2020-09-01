39e58bf14b023a13d2195d4f4056468937089d6e
Harshalshree <harshalshree03@gmail.com>
2020-09-01 16:22:16 +0530

    Removed Node Modules | Initial Commit for Main Project

--- testback/index.js
+++ testback/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000
+const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/login', (req, res) =>{
+  res.send('You are visiting a login route');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () =>{
+  console.log('Listening at port 8000');
+});
+
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
