38f3da02ba27bb02ec679ad279cb5a56c8f4df9f
Harshalshree <harshalshree03@gmail.com>
2020-10-01 22:48:10 +0530

    Middleware Testing

--- testback/index.js
+++ testback/index.js
@@ -8,6 +8,10 @@ app.get('/login', (req, res) =>{
   res.send('You are visiting a login route');
 });
 
+app.get('/admin', (req, res) => {
+  return res.send("This is admin");
+})
+
 app.listen(port, () =>{
   console.log('Listening at port 8000');
 });
