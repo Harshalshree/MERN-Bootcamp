e7c33eb3f9871b219b536e3005f2947907383ddd
Harshalshree <harshalshree03@gmail.com>
2020-10-02 18:00:29 +0530

    Middleware Tested

--- testback/index.js
+++ testback/index.js
@@ -4,13 +4,25 @@ const app = express();
 
 const port = 8000;
 
+const admin = (req, res) => {
+  res.send("This is admin dashboard")
+}
+
+const isAdmin = (req, res, next) => {
+  console.log("Inside isAdmin Function");
+  next();
+};
+
+const isLoggedIn = (req, res, next) => {
+  console.log("Inside isLoggedIn Function");
+  next();
+};
+
 app.get('/login', (req, res) =>{
   res.send('You are visiting a login route');
 });
 
-app.get('/admin', (req, res) => {
-  return res.send("This is admin");
-})
+app.get('/admin', isLoggedin, isAdmin, admin);
 
 app.listen(port, () =>{
   console.log('Listening at port 8000');
