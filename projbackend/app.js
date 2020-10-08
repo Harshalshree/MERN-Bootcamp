8468ba4eba9f142b346a721642e3872ae7bba14a
Harshalshree <harshalshree03@gmail.com>
2020-10-08 13:47:23 +0530

    Basic Auth Route Created

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const authRoutes = require("./routes/auth")
+
 //Connecting to database
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true, 
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,3 +1,13 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const signout = (req, res) => {
+    res.json({
+        message : "User Signed Out",
+        
+    })
+}
+
+router.get("/signout", signout)
+
+module.exports = router
