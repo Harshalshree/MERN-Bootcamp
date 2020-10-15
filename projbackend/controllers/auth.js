d59e36567d448f10e56a4f690d32407455281b2d
Harshalshree <harshalshree03@gmail.com>
2020-10-15 21:42:47 +0530

    Added Signin Route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,13 @@
+exports.signup = (req, res) => {
+    res.json({
+        message: "Signup route works",
+    })
+}
+
 exports.signout = (req, res) => {
     res.json({
         message : "User Signed Out",
         
     })
 }
+
