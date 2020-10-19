1b1df1ee919f465eb31e740eebb8c2a07aa2ad7a
Harshalshree <harshalshree03@gmail.com>
2020-10-19 14:11:20 +0530

    Validation in routes setup done

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
+const { check } = require('express-validator');
 const {signout, signup} = require("../controllers/auth")
 
-router.post("/signup", signup)
+router.post("/signup", [
+    check("name").isLength({min:2}).withMessage("Name should be at least 2 characters"),
+    check("email").isEmail().withMessage("Valid email is required"),
+    check("password").isLength({min:3}).withMessage("Password should be at least 3 characters is"),
+], signup)
 router.get("/signout", signout)
 
 module.exports = router
