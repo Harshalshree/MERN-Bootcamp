cc2bb808ecaeb700e24bd899d605d65365d38c9a
Harshalshree <harshalshree03@gmail.com>
2020-11-20 17:23:23 +0530

    Bug Fix to hide sensitive info in getUser method added

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -13,6 +13,10 @@ exports.getUserByID = (req,res,next,id) => {
 }
 
 exports.getUser = (req,res) => {
-    //TODO: get back here for password
+    req.profile.salt = undefined
+    req.profile.encry_password = undefined
+    req.profile.createdAt = undefined
+    req,profile.updatedAt = undefined
+
     return res.json(req.profile)
 }
