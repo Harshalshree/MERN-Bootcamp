c25ed61bc3799ce14b1dbd66659ef5f6d7e5b182
Harshalshree <harshalshree03@gmail.com>
2020-12-07 13:15:58 +0530

    getOrderStatus and updateStatus functions added

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -4,13 +4,16 @@ var router = express.Router()
 const {getUserById, pushOrderInPurchaseList} = require("../controllers/user")
 const {isAdmin, isAuthenticated, isSignedIn} = require("../controllers/auth")
 const {updateStock} = require("../controllers/product")
-const {getOrderById, createOrder, getAllOrders} = require("../controllers/order")
+const {getOrderById, createOrder, getAllOrders, updateStatus, getOrderStatus} = require("../controllers/order")
 
 router.param("userId", getUserById)
 router.param("orderId", getOrderById)
 
 router.post("/order/create/:userId", isSignedIn, isAuthenticated,pushOrderInPurchaseList,updateStock, createOrder)
 
-router.get("order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders)
+router.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders)
+router.get("/order/status/:userId", isSignedIn, isAuthenticated, isAdmin, getOrderStatus)
+
+router.put("/order/:orderId/status/:userId", isSignedIn, isAuthenticated, isAdmin, updateStatus)
 
 module.exports = router
