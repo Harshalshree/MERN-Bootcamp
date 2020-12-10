f4332ffef072bce7999e1e154eb667dea94c2cf7
Harshalshree <harshalshree03@gmail.com>
2020-12-10 15:07:59 +0530

    React todo app: Static UI created

--- testfront/reactjs/todo/src/App.js
+++ testfront/reactjs/todo/src/App.js
@@ -7,7 +7,21 @@ class App extends React.Component {
     return(
       <div>
         <img src={logo} className="App-logo"/>
-        <h1 className="App-title">ToDo App</h1>
+        <h1 className="app-title">ToDo App</h1>
+        <div className="container">
+          <br/>
+          <input type="text" className="input-text" placeholder="Write a ToDo"/>
+          <button className="add-btn">Add ToDo</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox"/>
+                Record youtube videos
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
