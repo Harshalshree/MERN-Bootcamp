1b582ab03371c7ece0e95144725732068f9e2bdb
Harshalshree <harshalshree03@gmail.com>
2021-02-03 15:25:43 +0530

    React Router Crash Course Completed

---
+++ testfront/reactjs/reactrouter/src/Notfound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notfound(){
+    return(
+        <div>
+            <h1>Page not found Component</h1>
+        </div>
+    )
+}
--- testfront/reactjs/reactrouter/src/index.js
+++ testfront/reactjs/reactrouter/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Link, Switch, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import User from './User';
+import Notfound from './Notfound';
+import Visit from './Visit';
 
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/user">User</Link></li>
+        <li><Link to="/visit">Visit</Link></li>
+      </ul>
+    </div>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/user" component={User} />
+      <Route path="/visit" component={Visit} />
+      <Route component={Notfound}/>
+    </Switch>
+  </Router>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
