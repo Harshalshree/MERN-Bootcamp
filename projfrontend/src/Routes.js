550977a14640e3cc2e772108a3cec759b5c7d1d0
Harshalshree <harshalshree03@gmail.com>
2021-02-18 21:26:41 +0530

    Bootstrap & FontAwesome CDNs added

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './core/Home'
 
+
 export default function Routes(){
     return(
         <BrowserRouter>
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles.css"
 
 export default function Home(){
     return(
         <div>
-            <h1>Hello Home</h1>
+            <h1 className="text-white">Hello Home</h1>
         </div>
     )
 }
