9ed36bb0d8ee6004d596a9c3651855afdb608f5a
Harshalshree <harshalshree03@gmail.com>
2021-02-22 15:38:43 +0530

    Base component added

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Base({
+    title = "My Title",
+    description = "My Description",
+    className = "bg-dark text-white p-4",
+    children
+}){
+    return(
+        <div>
+            <div className="container-fluid">
+                <div className="jumbotron bg-dark text-white text-center">
+                    <h2 className="display-4">{title}</h2>
+                    <p className="lead">{description}</p>
+                </div>
+                <div className={className}>
+                    {children}
+                </div>
+            </div>
+            <footer className="footer bg-dark mt-auto py-3">
+                <div className="container-fluid bg-success text-white text-center py-3">
+                    <h4>If you have any questions, feel free to reach out!</h4>
+                    <button className="btn btn-warning btn-lg">Contact Us</button>
+                </div>
+                <div className="container">
+                    <span className="text-muted">
+                        <span className="text-white">MERN</span> Bootcamp Project
+                    </span>
+                </div>
+                <div className="container"></div>
+            </footer>
+        </div>
+    )
+}
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../styles.css"
 import {API} from "../backend"
+import Base from './Base'
 
 export default function Home(){
     return(
-        <div>
-            <h1 className="text-white">Hello {API}</h1>
-        </div>
+        <Base title="HomePage">
+            <h1 className="text-white">Hello World</h1>
+        </Base>
     )
 }
