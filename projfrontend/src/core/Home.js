88c8f33d22badd291339ab38990fde747ed0aec2
Harshalshree <harshalshree03@gmail.com>
2021-02-22 14:30:50 +0530

    Backend API info added

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "../styles.css"
+import {API} from "../backend"
 
 export default function Home(){
     return(
         <div>
-            <h1 className="text-white">Hello Home</h1>
+            <h1 className="text-white">Hello {API}</h1>
         </div>
     )
 }
