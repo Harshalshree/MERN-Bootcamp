1b5600dc7171c898d5f547fe299e2e60ee587340
Harshalshree <harshalshree03@gmail.com>
2021-02-24 01:01:36 +0530

    Basic navbar added

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from './navbar'
 
 export default function Base({
     title = "My Title",
@@ -8,6 +9,9 @@ export default function Base({
 }){
     return(
         <div>
+            <Navbar>
+                
+            </Navbar>
             <div className="container-fluid">
                 <div className="jumbotron bg-dark text-white text-center">
                     <h2 className="display-4">{title}</h2>
---
+++ projfrontend/src/core/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link, withRouter} from 'react-router-dom'
+
+function Navbar(){
+    return(
+        <div>
+            <ul className="nav nav-tabs bg-dark">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Dashboard</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Cart</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">A.Dashboard</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Signup</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Signin</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Signout</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default withRouter(Navbar)
