5eda72aa99704db3b794304d58e0421c4aea04d1
Harshalshree <harshalshree03@gmail.com>
2021-03-16 15:02:23 +0530

    Frontend: Basic signup route created

---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import Base from '../core/Base'
+import {Link} from 'react-router-dom'
+
+const Signup = ()=>{
+    return(
+        <Base>
+        <h1>Signup</h1>
+        </Base>
+    )
+}
--- testback/index.js
+++ testback/index.js
@@ -28,12 +28,3 @@ app.listen(port, () =>{
   console.log('Listening at port 8000');
 });
 
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
