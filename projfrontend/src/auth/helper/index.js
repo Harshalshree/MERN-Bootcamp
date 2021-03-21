841b1f317d330bbd054322879dd109a32d12d568
Harshalshree <harshalshree03@gmail.com>
2021-03-21 14:51:53 +0530

    Frontend: authenticate and signout auth helpers added

--- projfrontend/src/auth/helper/index.js
+++ projfrontend/src/auth/helper/index.js
@@ -35,4 +35,26 @@ export const signIn = (user) => {
     })
 }
 
+export const authenticate = (data, next) =>{
+    if(typeof window !== "undefined"){
+        localStorage.setItem("jwt", JSON.stringify(data))
+        next()
+    }
+}
+
+export const signOut = next => {
+    if(typeof window !== "undefined"){
+        localStorage.removeItem("jwt")
+        next()
+        return fetch(`${API}/signout`,{
+            method:"GET"
+        })
+        .then(response=>{
+            console.log("signout success")
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+}
 
