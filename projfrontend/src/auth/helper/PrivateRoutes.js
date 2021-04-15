d7d0317ece7894d28b86f25df3a53a9ee0ae6508
Harshalshree <harshalshree03@gmail.com>
2021-04-15 21:23:44 +0530

    Initial Private Routes Code Added

---
+++ projfrontend/src/auth/helper/PrivateRoutes.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Router, Redirect} from 'react-router-dom'
+import {isAuthenticated} from './index'
+
+function PrivateRoute({ children, ...rest }) {
+    let auth = useAuth();
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          auth.user ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
+  
