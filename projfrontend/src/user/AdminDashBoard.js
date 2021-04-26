1306ba37792ae9a24d708eb09082b552cea73e19
Harshalshree <harshalshree03@gmail.com>
2021-04-26 17:15:04 +0530

    Frontend: UI elements in Admin Dashboard added

--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Base from '../core/Base'
+import { isAuthenticated } from '../auth/helper/index'
 
 const AdminDashboard = () => {
+    const {user: {name, email, role}} = isAuthenticated()
+
+    const adminLeftSide = () => {
+        return(
+            <div className="card"></div>
+        )
+    }
+
+    const adminRightSide = () => {
+
+    }
+
     return(
-        <Base title="AdminDashboard Page">
-        <h1>This is AdminDashboard page</h1>
+        <Base title="Welcome to Admin Page" 
+        description="Manage all of your products here"
+        >
+        {adminLeftSide()}
+        {adminRightSide()}
         </Base>
     )
 }
