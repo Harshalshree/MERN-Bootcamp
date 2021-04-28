4e8ca09bfdfa96ece98c8e0d505720ae3dd7ad5d
Harshalshree <harshalshree03@gmail.com>
2021-04-28 16:16:53 +0530

    Frontend: CreateProduct API Call added

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -1,7 +1,6 @@
 import {API} from '../../backend'
 
-
-
+//category calls
 export const createCategory = (userId, token, category) => {
     return fetch(`${API}/category/create/${userId}`,{
         method: "POST",
@@ -16,4 +15,23 @@ export const createCategory = (userId, token, category) => {
         return response.json()
     })
     .catch(err => console.log(err))
+}
+
+//product calls
+
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method:"POST",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        },
+        body: product
+    })
+    .then(res => {
+        return res.json()
+    })
+    .catch(err => {
+        console.log(err);
+    })
 }
