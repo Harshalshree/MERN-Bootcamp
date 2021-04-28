b008672643b56e17443cd9ce26bb880a14426e63
Harshalshree <harshalshree03@gmail.com>
2021-04-28 16:56:09 +0530

    Frontend: UpdateProduct & DeleteProduct API Calls added

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -70,3 +70,37 @@ export const getProduct = (productId) => {
         console.log(err);
     })
 }
+
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method:"PUT",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        },
+        body: product
+    })
+    .then(res => {
+        return res.json()
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+export const deleteProduct = (productId, userId, token) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method:"DELETE",
+        headers:{
+            Accept: "application/json",
+            Authorization: `Bearer ${token}` 
+        },
+        body: product
+    })
+    .then(res => {
+        return res.json()
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
