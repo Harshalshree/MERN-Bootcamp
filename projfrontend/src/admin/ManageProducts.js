224b3c4b6db67f8e94beb2ab17e4779b91174bc4
Harshalshree <harshalshree03@gmail.com>
2021-05-06 15:04:09 +0530

    Frontend: ImageHelper Component Created

--- projfrontend/src/admin/ManageProducts.js
+++ projfrontend/src/admin/ManageProducts.js
@@ -54,7 +54,7 @@ const ManageProducts = () => {
                     <div className="col-4">
                     <Link
                         className="btn btn-success"
-                        to={`/admin/product/update/${productId}`}
+                        to={`/admin/product/update/${product._id}`}
                     >
                         <span className="">Update</span>
                     </Link>
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,11 +2,23 @@ import React from 'react'
 import "../styles.css"
 import {API} from "../backend"
 import Base from './Base'
+import Card from './Card'
+
 
 export default function Home(){
     return(
         <Base title="HomePage" description="Welcome to the TShirt Store">
-            <h1 className="text-white">Hello World</h1>
+            <div className="row">
+                <div className="col-4">
+                    <Card/>
+                </div>
+                <div className="col-4">
+                    
+                </div>
+                <div className="col-4">
+                    
+                </div>
+            </div>
         </Base>
     )
 }
