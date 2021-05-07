9131d80810f1bd78e104f60cda71e7cabb47f746
Harshalshree <harshalshree03@gmail.com>
2021-05-07 16:18:02 +0530

    Frontend: Adding Products to localStorage completed

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Redirect } from 'react-router';
+import { addItemToCart } from './helper/CartHelper';
 import ImageHelper from './helper/ImageHelper';
+
 
 
 
@@ -9,6 +12,23 @@ const Card = ({
     removeFromCart = false,
 }) => {
 
+    const [redirect, setRedirect] = useState(false);
+    const [count, setCount] = useState(product.count)
+
+    const addInCart = () => {
+        addItemToCart(product, () => {
+            setRedirect(true)
+        })
+    }
+
+    const getARedirect = (redirect) => {
+        if(redirect){
+            return(
+                <Redirect to="/cart"/>
+            )
+        }
+    }
+
     const cardTitle = product ? product.name : "A photo from pexels"
     const cardDescription = product ? product.description : "-"
     const cardPrice = product ? product.price : "0"
@@ -17,7 +37,7 @@ const Card = ({
         if(addToCart){
             return(
                 <button
-                onClick={() => {}}
+                onClick={()=> {addInCart()}}
                 className="btn btn-block btn-outline-success mt-2 mb-2"
                 >
                 Add to Cart
@@ -41,6 +61,7 @@ const Card = ({
         <div className="card text-white bg-dark border border-info ">
         <div className="card-header lead">{cardTitle}</div>
         <div className="card-body">
+            {getARedirect(redirect)}
             <ImageHelper product={product}/>
             <p className="lead bg-success font-weight-normal text-wrap">
             {cardDescription}
@@ -48,7 +69,7 @@ const Card = ({
             <p className="btn btn-success rounded  btn-sm px-4">$ {cardPrice}</p>
             <div className="row">
             <div className="col-12">
-                {showAddToCart(addToCart)}
+                {showAddToCart(addInCart)}
             </div>
             <div className="col-12">
                 {showRemoveFromCart(removeFromCart)}
